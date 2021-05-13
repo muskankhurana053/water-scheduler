2537bff494571af092a4a6a01bd8abdbb9cc02c8
muskankhurana053 <muskankhurana053@gmail.com>
2021-05-13 17:55:21 +0530

    second commit

--- scdule.py
+++ scdule.py
@@ -2,7 +2,7 @@ import time
 from plyer import notification
 while True:
     notification.notify(
-    title="please drink water now",
+    title="please drink water now!!",
     message=" The U.S. National Academies of Sciences, Engineering, and Medicine determined that an adequate daily fluid intake is: About 15.5 cups (3.7 liters) of fluids a day for men. About 11.5 cups (2.7 liters) of fluids a day for women."
     time.timeout=10,
     app_icon='C:\\Users\\muskan\\Desktop\\DS_101\\Iconsmind-Outline-Glass-Water.ico'
